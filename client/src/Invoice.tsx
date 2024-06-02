021868538123d144028d0308f385c7aead7b8618
Mayank Diwate <MayankDiwate007@gmail.com>
2024-06-02 11:40:53 +0530

    Minor UI changes in Invoice PDF

--- client/src/Invoice.tsx
+++ client/src/Invoice.tsx
@@ -33,6 +33,7 @@ const styles = StyleSheet.create({
   table: {
     display: "flex",
     width: "auto",
+    marginTop: 50,
     marginBottom: 20,
   },
   tableRow: {
@@ -49,9 +50,11 @@ const styles = StyleSheet.create({
   },
   terms: {
     marginTop: 20,
-    padding: 10,
-    backgroundColor: "#EEE",
-    borderRadius: 5,
+    padding: 20,
+    paddingHorizontal: 40,
+    backgroundColor: "black",
+    color: "white",
+    borderRadius: 50,
   },
 });
 
@@ -77,54 +80,126 @@ export const Invoice = ({
   return (
     <Document>
       <Page style={styles.page}>
-        <View style={styles.header}>
-          <Text style={styles.title}>INVOICE GENERATOR</Text>
-          <Text>levitation infotech</Text>
-        </View>
-        <Text>Sample Output should be this</Text>
-        <View style={styles.table}>
-          <View style={[styles.tableRow, styles.tableHeader]}>
-            <Text style={styles.tableCell}>Product</Text>
-            <Text style={styles.tableCell}>Qty</Text>
-            <Text style={styles.tableCell}>Rate</Text>
-            <Text style={styles.tableCell}>Total</Text>
-          </View>
-          {products.map((product, index) => (
-            <View key={index} style={styles.tableRow}>
-              <Text style={styles.tableCell}>{product.name}</Text>
-              <Text style={styles.tableCell}>{product.quantity}</Text>
-              <Text style={styles.tableCell}>{product.rate}</Text>
-              <Text style={styles.tableCell}>
-                INR {product.quantity * product.rate}
-              </Text>
-            </View>
-          ))}
-        </View>
         <View
           style={{
             display: "flex",
             flexDirection: "column",
-            justifyContent: "flex-end",
-            marginBottom: 20,
+            justifyContent: "space-between",
+            height: "100%",
           }}
         >
-          <Text>Total: INR {total}</Text>
-          <Text>GST: {gst}%</Text>
-          <Text style={{ fontWeight: "bold" }}>
-            Grand Total: INR {grandTotal}
-          </Text>
-        </View>
-        <Text>
-          Valid until: {validUntil}/{new Date().getUTCMonth()}/
-          {new Date().getUTCFullYear()}
-        </Text>
-        <View style={styles.terms}>
-          <Text>Terms and Conditions</Text>
-          <Text>
-            We are happy to supply any further information you may need and
-            trust that you call on us to fill your order, which will receive our
-            prompt and careful attention
-          </Text>
+          <View>
+            <View style={styles.header}>
+              <Text style={styles.title}>INVOICE GENERATOR</Text>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "flex-start",
+                  gap: 5,
+                  marginRight: 20,
+                }}
+              >
+                <Text>levitation</Text>
+                <Text>infotech</Text>
+              </View>
+            </View>
+            <View style={styles.table}>
+              <View style={[styles.tableRow, styles.tableHeader]}>
+                <Text style={styles.tableCell}>Product</Text>
+                <Text style={styles.tableCell}>Qty</Text>
+                <Text style={styles.tableCell}>Rate</Text>
+                <Text style={styles.tableCell}>Total</Text>
+              </View>
+              {products.map((product, index) => (
+                <View key={index} style={styles.tableRow}>
+                  <Text style={styles.tableCell}>{product.name}</Text>
+                  <Text style={styles.tableCell}>{product.quantity}</Text>
+                  <Text style={styles.tableCell}>{product.rate}</Text>
+                  <Text style={styles.tableCell}>
+                    INR {product.quantity * product.rate}
+                  </Text>
+                </View>
+              ))}
+            </View>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-end",
+                justifyContent: "flex-end",
+                marginBottom: 20,
+              }}
+            >
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  width: "40%",
+                }}
+              >
+                <Text style={{ fontWeight: "bold" }}>Total: INR </Text>
+                <Text>{total}</Text>
+              </View>
+              <Text
+                style={{
+                  width: "40%",
+                  height: "1",
+                  backgroundColor: "#EEE",
+                  marginTop: 10,
+                  marginBottom: 10,
+                }}
+              />
+              <view
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  width: "40%",
+                }}
+              >
+                <Text>GST (18%):</Text>
+                <Text>INR {gst}</Text>
+              </view>
+              <Text
+                style={{
+                  width: "40%",
+                  height: "1",
+                  backgroundColor: "#EEE",
+                  marginTop: 10,
+                  marginBottom: 10,
+                }}
+              />
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  fontWeight: "bold",
+                  width: "40%",
+                }}
+              >
+                <Text style={{ fontWeight: "bold" }}>Grand Total:</Text>
+                <Text>INR {grandTotal}</Text>
+              </View>
+            </View>
+          </View>
+          <View style={{ display: "flex", flexDirection: "row", gap: 6 }}>
+            <Text>Valid until:</Text>
+            <Text style={{ fontWeight: "bold" }}>
+              {validUntil}/{new Date().getUTCMonth()}/
+              {new Date().getUTCFullYear()}
+            </Text>
+          </View>
+          <View style={styles.terms}>
+            <Text style={{ fontWeight: "bold" }}>Terms and Conditions</Text>
+            <Text>
+              We are happy to supply any further information you may need and
+              trust that you call on us to fill your order, which will receive
+              our prompt and careful attention
+            </Text>
+          </View>
         </View>
       </Page>
     </Document>
