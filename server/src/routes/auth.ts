52ac68073d5030c8bbf4f1ed57d9d6af5de02af2
Mayank Diwate <mayankdiwate007@gmail.com>
2024-11-07 21:51:42 +0530

    minor fix

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import { signin, signout, signup } from "../controllers/auth.controller";
-import verifyToken from "../utils/verifyToken";
 
 const router = express.Router();
 
 router.post("/signin", signin);
 router.post("/signup", signup);
-router.post("/signout", verifyToken, signout);
+router.post("/signout", signout);
 
 export default router;
